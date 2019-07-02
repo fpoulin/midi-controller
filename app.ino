e82a2f565dfdab9b9a754b631fbaa05059a46224
Florian Poulin <florian.poulin@zyllem.com>
2019-07-02 23:30:38 +0800

    Midi clock sync

--- app.ino
+++ app.ino
@@ -3,20 +3,55 @@
 
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial, MIDI);
 
+bool playing = false;
+int ticks = 0;
 int count = 0;
+
+void handleClock(void)
+{
+  if (playing && ticks++ % 6 == 0)
+  {
+    doTheThing();
+  }
+}
+
+void handleStart(void)
+{
+  playing = true;
+  ticks = 0;
+  count = 0;
+}
+
+void handleContinue(void)
+{
+  playing = true;
+  ticks = 0;
+}
+
+void handleStop(void)
+{
+  playing = false;
+  ticks = 0;
+}
+
+void doTheThing()
+{
+  int note = count++ % 16 + 36;
+  MIDI.sendNoteOn(note, 127, 1);
+  MIDI.sendNoteOn(note-1, 0, 1);
+}
 
 void setup()
 {
   MIDI.begin(MIDI_CHANNEL_OMNI); // Listen to all incoming messages
-  pinMode(LED_BUILTIN, OUTPUT);
+  MIDI.turnThruOff();
+  MIDI.setHandleClock(handleClock);
+  MIDI.setHandleStart(handleStart);
+  MIDI.setHandleContinue(handleContinue);
+  MIDI.setHandleStop(handleStop);
 }
 
 void loop()
 {
-  int note = count++ % 25 + 36;
-
-  MIDI.sendNoteOn(note, 127, 1);
-  delay(100);
-  MIDI.sendNoteOn(note, 0, 1);
-  delay(100);
+  MIDI.read();
 }
