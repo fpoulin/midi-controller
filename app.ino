a74ecb270115fe36cd3d960918f1e70a1a29b6b9
Florian Poulin <florian.poulin@zyllem.com>
2019-07-02 21:51:30 +0800

    Midi test

--- app.ino
+++ app.ino
@@ -1,47 +1,22 @@
 #define ARDUINO 1000
-#include <LedControl.h>
+#include <MIDI.h>
 
-LedControl lc1 = LedControl(2, 4, 3, 4);
-LedControl lc2 = LedControl(5, 7, 6, 4);
-unsigned long delaytime = 10;
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial, MIDI);
 
-// the setup function runs once when you press reset or power the board
+int count = 0;
+
 void setup()
 {
-  for (int address = 0; address < lc1.getDeviceCount(); address++) {
-    lc1.shutdown(address, false);
-    lc1.setIntensity(address, 8);
-    lc1.clearDisplay(address);
-  }
-
-  for (int address = 0; address < lc2.getDeviceCount(); address++) {
-    lc2.shutdown(address, false);
-    lc2.setIntensity(address, 8);
-    lc2.clearDisplay(address);
-  }
+  MIDI.begin(MIDI_CHANNEL_OMNI); // Listen to all incoming messages
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-// the loop function runs over and over again forever
 void loop()
 {
-  //read the number cascaded devices
-  int devices = lc1.getDeviceCount();
+  int note = count++ % 25 + 36;
 
-  int count = 0;
-  for (int i = 0; i < devices; i++)
-  {
-    int deviceNb = i;
-    for (int j = 0; j < 8; j++)
-    {
-      int col = 7-j;
-      double radFactor = 3.1415926535 / 180;
-      int row = floor((sin(radFactor * (i * 8 + 7-col) / 32.0 * 360) + 1) / 2 * 7.99);
-
-      lc1.setLed(deviceNb, row, col, true);
-      lc2.setLed(deviceNb, row, col, true);
-      delay(delaytime);
-      lc1.setLed(deviceNb, row, col, false);
-      lc2.setLed(deviceNb, row, col, false);
-    }
-  }
+  MIDI.sendNoteOn(note, 127, 1);
+  delay(100);
+  MIDI.sendNoteOn(note, 0, 1);
+  delay(100);
 }
